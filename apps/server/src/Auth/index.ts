913bcde86cdd9d80d46e2b7d88223df20eab3ba0
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-02 03:04:00 +0530

    Login Endpoint

--- apps/server/src/Auth/index.ts
+++ apps/server/src/Auth/index.ts
@@ -3,86 +3,89 @@ import { createUserSchema, loginUserSchema } from "../types/index.js";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 
-
 const prismaClient = new PrismaClient();
 
-
-export const AuthRouter:Router = express.Router();
+export const AuthRouter: Router = express.Router();
 
 AuthRouter.get("/health", (req, res) => {
-    res.status(200).json({
-        Message: "Auth Endpoint Up and Running"
-    });
+  res.status(200).json({
+    Message: "Auth Endpoint Up and Running",
+  });
 });
 
 AuthRouter.post("/signup", async (req, res) => {
-    const parsedData = createUserSchema.safeParse(req.body);
+  const parsedData = createUserSchema.safeParse(req.body);
 
-    if(!parsedData.success){
-        return res.status(403).json({
-            Message: "Invalid Input",
-            error: parsedData.error.format(),
-        });
-    }
+  if (!parsedData.success) {
+    return res.status(403).json({
+      Message: "Invalid Input",
+      error: parsedData.error.format(),
+    });
+  }
 
-    try{
-        const {name, email, password} = parsedData.data;
-        const hashedPassword = await bcrypt.hash(password, 10);
+  try {
+    const { name, email, password } = parsedData.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const User = await prismaClient.user.create({
-            data:{
-                name: name,
-                email: email,
-                password: hashedPassword
-            }
-        });
+    const User = await prismaClient.user.create({
+      data: {
+        name: name,
+        email: email,
+        password: hashedPassword,
+      },
+    });
 
-        return res.status(201).json({
-            Message: "user Created Successfully"
-        });
-    } catch(error){
-        return res.status(500).json({
-            Message: "Something went wrong",
-            error: String(error)
-        });
-    }
-
-
+    return res.status(201).json({
+      Message: "user Created Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      Message: "Something went wrong",
+      error: String(error),
+    });
+  }
 });
 
 AuthRouter.post("/login", async (req, res) => {
-    const parsedData = loginUserSchema.safeParse(req.body);
+  const parsedData = loginUserSchema.safeParse(req.body);
 
-   try{
-     if(!parsedData.success){
-        return res.status(411).json({
-            Message: "Invalid Input",
-            error: parsedData.error.format(),
-        });
+  try {
+    if (!parsedData.success) {
+      return res.status(411).json({
+        Message: "Invalid Input",
+        error: parsedData.error.format(),
+      });
     }
 
-    const {email, password} = parsedData.data;
+    const { email, password } = parsedData.data;
 
-   const user = prismaClient.user.findFirst({
-    where:{
-        email: email
-    }
-
-   });
-
-   if(!user){
-    return res.status(404).json({
-        Message: "User Not Found"
+    const user = await prismaClient.user.findFirst({
+      where: {
+        email: email,
+      },
     });
-   }
 
-   
+    if (!user) {
+      return res.status(404).json({
+        Message: "User Not Found",
+      });
+    }
 
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-   } catch(error){
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        Message: "Incorrect password",
+      });
+    }
+
+    return res.status(200).json({
+      Message: "Login Successful",
+    });
+  } catch (error) {
     return res.status(500).json({
-        Message: "Something went wrong",
-        error: String(error)
-    })
-   }
+      Message: "Something went wrong",
+      error: String(error),
+    });
+  }
 });
