f2a93ca518d5b5685cd3b0152b4a2759c3025aad
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-02 16:45:11 +0530

    roleMiddleware

--- apps/server/src/middleware/index.ts
+++ apps/server/src/middleware/index.ts
@@ -1,5 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
-import jwt, { type JwtPayload }  from "jsonwebtoken"
+import jwt, { type JwtPayload } from "jsonwebtoken";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,29 +7,61 @@ dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 interface AuthenticatedRequest extends Request {
-    user? : string | JwtPayload
+  user?: string | JwtPayload;
 }
 
-export function middleware(req: AuthenticatedRequest, res: Response, next: NextFunction){
-    const authHeader = (req.headers["authorization"] as string ) ?? "";
+export function authMiddleware(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = (req.headers["authorization"] as string) ?? "";
 
-    if(!authHeader){
-        return res.status(401).json({
-            Message: "No token Provided"
-        });
+  if (!authHeader) {
+    return res.status(401).json({
+      Message: "No token Provided",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token!, JWT_SECRET as string);
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({
+      Message: "Invalid or expired token",
+    });
+  }
+}
+
+export function roleMiddleware(req: AuthenticatedRequest,res: Response,next: NextFunction) {
+  const authHeader = (req.headers["authorization"] as string) ?? "";
+
+  if (!authHeader) {
+    return res.status(401).json({
+      Message: "No token Provided",
+    });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token!, JWT_SECRET as string) as JwtPayload;
+
+    if (decoded.role && decoded.role === "ADMIN") {
+      next();
+      return;
     }
 
-    const token = authHeader.split(" ")[1];
-
-    try{
-        const decoded = jwt.verify(token!, JWT_SECRET as string);
-
-        req.user = decoded;
-        next();
-
-    } catch(error){
-        return res.status(403).json({
-            Message: "Invalid or expired token"
-        });
-    }
+    return res.status(403).json({
+        Message: "Access denied. Admins only"
+    });
+  } catch (error) {
+    return res.status(403).json({
+      Message: "Invalid or expired token",
+    });
+  }
 }
