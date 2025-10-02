61780eee1a754c5faaaa4d0aef63068158aaaf87
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-02 17:03:01 +0530

    products Schema

---
+++ apps/server/src/products/index.ts
@@ -0,0 +1,9 @@
+import express, { Router } from "express"
+
+export const productRouter:Router = express.Router();
+
+productRouter.get("/health", (req, res) => {
+    res.status(200).json({
+        Message: "Products Health End Point"
+    });
+});
