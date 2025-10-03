cc15eb963c77e132df8f87f23cb108519afcd049
Mohfazam <mohd.sarwar.code@gmail.com>
2025-10-03 17:20:53 +0530

    Clean Up

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,7 @@
 import express, {Router}  from "express"
 import { MainRouter } from "./Routes/index.js";
 import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-import swaggerJsdoc from "swagger-jsdoc";
+
 
 
 const app = express();
@@ -18,27 +17,6 @@ app.get("/Health", (req, res) => {
 });
 
 app.use("/api/v1", MainRouter);
-
-const swaggerOptions = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Prime Trade AI API",
-      version: "1.0.0",
-      description: "API documentation for Prime Trade AI backend",
-    },
-    servers: [
-      {
-        url: "https://prime-trade-ai-server.vercel.app/api/v1", 
-      },
-    ],
-  },
-  apis: ["./Routes/*.ts"], 
-};
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 app.listen(3001, () => {
